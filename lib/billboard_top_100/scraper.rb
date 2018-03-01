ed0cfd5fcde6f2e87ba909ee94f6af9f53109223
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-03-01 12:33:57 -0800

    Scraper.scrape_top_100 now working

--- lib/billboard_top_100/scraper.rb
+++ lib/billboard_top_100/scraper.rb
@@ -2,15 +2,22 @@ class Scraper
 
 
 	def self.scrape_top_100(url)
-		doc = Nokogiri::HTML(open(url))
 
-		#current_rank = doc.search(".chart-row__current-week").text         #=> "1"
-		#previous_rank  = doc.search(".chart-row__last-week").text          #=> "Last Week: 2"
-		#song =  doc.search(".chart-row__song").text
-		#artist =  doc.search(".chart-row__artist").text.gsub("\n", "")
-		#weeks_on_chart = doc.search(".chart-row__weeks-on-chart span.chart-row__value").text
-		#peak_spot = doc.search(".chart-row__top-spot span.chart-row__value").text
-		binding.pry
+		@all = []
+
+		doc = Nokogiri::HTML(open(url)).search(".chart-data article.chart-row")
+
+		doc.each do |song|
+			song_hash = Hash.new 
+			song_hash[:current_rank] = song.search(".chart-row__current-week").text         #=> "1"
+			song_hash[:previous_rank]  = song.search(".chart-row__last-week").text          #=> "Last Week: 2"
+			song_hash[:song] =  song.search(".chart-row__song").text
+			song_hash[:artist] =  song.search(".chart-row__artist").text.gsub("\n", "")
+			song_hash[:weeks_on_chart] = song.search(".chart-row__weeks-on-chart span.chart-row__value").text
+			song_hash[:peak_spot] = song.search(".chart-row__top-spot span.chart-row__value").text
+			@all << song_hash
+			
+		end 
 	end 
 end 
 
